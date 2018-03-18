b0930b8dbcafdab51e5cbe463bf9adfec6fd0750
ramaniG <ramani_g@hotmail.com>
2018-03-18 22:22:04 +0800

    Finish implementation of straight-fwd api calls.

---
+++ thefmarketer/api/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import UsersList,UsersDetail
+from .views import ConsultantList,ConsultantDetail
+from .views import ConsultantCoverageList,ConsultantCoverageDetail
+from .views import ConsultantServicesList,ConsultantServicesDetail
+from .views import RequestList,RequestDetail
+from .views import ReviewList,ReviewDetail
+from .views import ChatList,ChatDetail
+
+urlpatterns = [
+    url(r'^users/$', UsersList.as_view()),
+    url(r'^user/(?P<pk>[0-9]+)/$', UsersDetail.as_view()),
+    url(r'^consultants/$', ConsultantList.as_view()),
+    url(r'^consultant/(?P<pk>[0-9]+)/$', ConsultantDetail.as_view()),
+    url(r'^coverages/$', ConsultantCoverageList.as_view()),
+    url(r'^coverage/(?P<pk>[0-9]+)/$', ConsultantCoverageDetail.as_view()),
+    url(r'^services/$', ConsultantServicesList.as_view()),
+    url(r'^service/(?P<pk>[0-9]+)/$', ConsultantServicesDetail.as_view()),
+    url(r'^requests/$', RequestList.as_view()),
+    url(r'^request/(?P<pk>[0-9]+)/$', RequestDetail.as_view()),
+    url(r'^reviews/$', ReviewList.as_view()),
+    url(r'^review/(?P<pk>[0-9]+)/$', ReviewDetail.as_view()),
+    url(r'^chats/$', ChatList.as_view()),
+    url(r'^chat/(?P<pk>[0-9]+)/$', ChatDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- thefmarketer/api/views.py
+++ thefmarketer/api/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from .models import Users,Consultant,ConsultantCoverage,ConsultantServices,Request,Review,Chat
+from .serializers import UsersSerializer,ConsultantSerializer,ConsultantCoverageSerializer,ConsultantServicesSerializer,RequestSerializer,ReviewSerializer,ChatSerializer
+from rest_framework import generics
 
-# Create your views here.
+'''Users'''
+class UsersList(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+class UsersDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+'''Consultant'''
+class ConsultantList(generics.ListCreateAPIView):
+    queryset = Consultant.objects.all()
+    serializer_class = ConsultantSerializer
+
+class ConsultantDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Consultant.objects.all()
+    serializer_class = ConsultantSerializer
+
+'''ConsultantCoverage'''
+class ConsultantCoverageList(generics.ListCreateAPIView):
+    queryset = ConsultantCoverage.objects.all()
+    serializer_class = ConsultantCoverageSerializer
+
+class ConsultantCoverageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ConsultantCoverage.objects.all()
+    serializer_class = ConsultantCoverageSerializer
+
+'''ConsultantServices'''
+class ConsultantServicesList(generics.ListCreateAPIView):
+    queryset = ConsultantServices.objects.all()
+    serializer_class = ConsultantServicesSerializer
+
+class ConsultantServicesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ConsultantServices.objects.all()
+    serializer_class = ConsultantServicesSerializer
+
+'''Request'''
+class RequestList(generics.ListCreateAPIView):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+
+class RequestDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+
+'''Review'''
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+'''Chat'''
+class ChatList(generics.ListCreateAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+
+class ChatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
